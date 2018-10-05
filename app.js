2503b8c7c61df7618e8b553cd95076ad1502595c
Astrocat2374 <sugiyama.evan@gmail.com>
2018-10-05 13:41:29 -1000

    Linked API and JS because stupid

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const input = document.querySelector('#name');
+var input = document.querySelector('#search').value;
+var submit = document.querySelector('#submit-form').addEventListener('click', displayInput);
 
-console.log('input', input);
+function displayInput(){
+    console.log('input', input);
+}
+
+axios
+    .get('https://rickandmortyapi.com/api/character/')
+    .then((response)=>{
+        console.log(response);
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
