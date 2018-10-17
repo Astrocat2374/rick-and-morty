d4089d284a034889481a2b76e7f783d0db7b77db
Astrocat2374 <sugiyama.evan@gmail.com>
2018-10-16 23:44:27 -1000

    Work on this tomorrow

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const input = document.querySelector('#search');
-const submit = document.querySelector('#submit-form').addEventListener('click', displayInput);
+const submit = document.querySelector('#submit-form').addEventListener('click', renderResults);
+/*const cardTemplate = `
+    <div class = 'card'>
+        <h2>${response.name}</h2>
+        <h3>${response.species}</h3>
+        <h3>${response.type}</h3>
+        <h3>${response.status}</h3>
+    </div
+`;*/
 
 function displayInput(){
     console.log('input', input.value);
@@ -18,8 +26,8 @@ function renderResults(){
     axios
      .get('https://rickandmortyapi.com/api/character')
      .then((response)=>{
-         console.log(
-            (response.data.results).search
-         );
-     })
+        console.log(input.value);
+        let displayName = (response.data.results).map(charname => charname.name.lastIndexOf(input.value));
+        console.log(displayName);
+    })
 }
